6ec3334a4794f614ab0115ccb3f531f5ebb18069
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2022-06-21 20:32:22 +0200

    Feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import Header from "./components/Header";
 
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
+const ContainerDiv = styled.div`
+  position: relative;
+  background-color: white;
+  height: 100vh;
 `;
 
 function App() {
   return (
-    <div>
-      <AppHeader>
-        <Header />
-      </AppHeader>
-    </div>
+    <ContainerDiv>
+      <Header />
+    </ContainerDiv>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,107 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../assets/Logo.png";
+import cartLogo from "../assets/shopping-cart.png";
+import arrow from "../assets/arrow-down-sign-to-navigate.png";
+
+const AppHeader = styled.header`
+  display: flex;
+  position: absolute;
+  background-color: white;
+  width: 100vw;
+  height: 7vh;
+`;
+
+const NavigationLinksDiv = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 5vw;
+`;
+
+const LogoDiv = styled.div`
+  display: flex;
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const CartCurrencyDiv = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: end;
+  margin-right: 5vw;
+  width: 100%;
+`;
+
+const NavigationLinks = styled.a`
+  &:link,
+  &:visited {
+    color: black;
+    padding: 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+  }
+
+  &:hover,
+  &:active {
+    color: #5ece7b;
+    border-bottom: 2px solid #5ece7b;
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: white;
+  &:hover {
+    cursor: pointer;
+    img {
+      opacity: 0.5;
+    }
+  }
+`;
+
+const DropDownArrow = styled.img`
+  width: 7px;
+  height: 7px;
+  margin-right: 1vw;
+`;
+
+const ImageLogo = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+const ImageCart = styled.img`
+  width: 22px;
+  height: 22px;
+`;
 
 export default function Header() {
   return (
     <div>
-      <h1>header</h1>
+      <AppHeader>
+        <NavigationLinksDiv>
+          <NavigationLinks href="url">WOMEN</NavigationLinks>
+          <NavigationLinks href="url">MEN</NavigationLinks>
+          <NavigationLinks href="url">KIDS</NavigationLinks>
+        </NavigationLinksDiv>
+
+        <LogoDiv>
+          <ImageLogo src={logo} alt="logo" />
+        </LogoDiv>
+
+        <CartCurrencyDiv>
+          <Button type="button">
+            <DropDownArrow src={arrow} alt="arrow" />
+          </Button>
+
+          <Button type="button">
+            <ImageCart src={cartLogo} alt="logo" />
+          </Button>
+        </CartCurrencyDiv>
+      </AppHeader>
     </div>
   );
 }
